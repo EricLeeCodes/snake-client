0632f7a90718b6cc6456ade3479abb8264883350
Eric Lee <ericleecodes@gmail.com>
2024-02-14 14:51:02 -0800

    Added client and play files for the snake game

---
+++ client.js
@@ -0,0 +1,26 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: '172.29.99.4',
+    port: 50541
+  });
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: ERL");
+    //conn.write("Move: up");
+    setInterval(() => {
+      conn.write("Move: up");
+    }, 50);
+  });
+
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+module.exports = connect;
